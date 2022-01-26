188cabc515df6173bb74885c1ec3d1b3b05c97a5
Peter Drake <drake@lclark.edu>
2022-01-26 14:39:51 -0800

    Wordle works for hard-coded word.

--- wordle.py
+++ wordle.py
@@ -1,24 +1,24 @@
 word = 'knoll'
-guess = input('Your guess: ')
 
-# Find letters in word that were not perfectly guessed
-imperfect = ''
-for i in range(5):
-    if word[i] == guess[i]:
-        pass
-    else:
-        imperfect += word[i]
-print(imperfect)
+for i in range(6):
+    guess = input('Your guess: ')
+    # Find letters in word that were not perfectly guessed
+    imperfect = ''
+    for j in range(5):
+        if not word[j] == guess[j]:
+            imperfect += word[j]
+    # Generate clue response
+    clue = ''
+    for j in range(5):
+        if word[j] == guess[j]:
+            clue += '!'
+        elif guess[j] in imperfect:
+            clue += '?'
+            imperfect = imperfect.replace(guess[j], '', 1)
+        else:
+            clue += '.'
+    print(clue)
+    if clue == '!!!!!':
+        exit()
 
-# Generate clue response
-correct = ''
-for i in range(5):
-    if word[i] == guess[i]:
-        correct += '!'
-    elif guess[i] in imperfect:
-        correct += '?'
-        imperfect = imperfect.replace(guess[i], '', 1)
-    else:
-        correct += '.'
-
-print(correct)
+print("You're out of guesses. The word was:", word)
