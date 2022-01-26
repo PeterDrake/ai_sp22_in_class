12fa0bbc002badd5aabbccdcfedf9a258396b8f6
Peter Drake <drake@lclark.edu>
2022-01-26 14:25:35 -0800

    Wordle now deals with correct letter in incorrect place.

--- wordle.py
+++ wordle.py
@@ -1,13 +1,23 @@
 word = 'knoll'
-
 guess = input('Your guess: ')
 
+# Find letters in word that were not perfectly guessed
+imperfect = ''
+for i in range(5):
+    if word[i] == guess[i]:
+        pass
+    else:
+        imperfect += word[i]
+print(imperfect)
+
+# Generate clue response
 correct = ''
 for i in range(5):
     if word[i] == guess[i]:
         correct += '!'
-    elif guess[i] in word:
+    elif guess[i] in imperfect:
         correct += '?'
+        imperfect = imperfect.replace(guess[i], '', 1)
     else:
         correct += '.'
 
